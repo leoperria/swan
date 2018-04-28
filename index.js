a5a34a19f1e9ef62a788c7e9b2e08ceba60e2b47
Leonardo Perria <leo.perria@stelltec.com>
2018-04-28 12:07:02 +0100

    Scrape member table

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const puppeteer = require("puppeteer");
 
 const NUM_RETRIES = 3;
-const userName = '';
-const password = '';
+const userName = 'dany.mura72@gmail.com';
+const password = '12345678';
 const win_width = 1200;
 const win_height = 1000;
 
@@ -17,8 +17,7 @@ const win_height = 1000;
         let args = [];
         args.push(`--window-size=${win_width},${win_height}`);
         const browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 10,
+            headless: true,
             args
         });
         const page = await browser.newPage();
@@ -33,6 +32,8 @@ const win_height = 1000;
         await page.type('input[name="login.Email"', userName);
         await page.type('input[name="login.Password"', password);
         await page.click('input#login');
+
+        await page.waitForSelector(".membershipTab");
         const loggedIn = (await page.$('.membershipTab')) !== null;
         if (!loggedIn) {
             await criticalException(`Can't login with user=${userName} and password=${password}`);
@@ -40,14 +41,53 @@ const win_height = 1000;
         }
         console.log("Logged in! ");
 
-        // Open timetable
+        await page.waitForSelector("#CSCAdmin");
         await clickLinkByText(page, "Class Timetable");
+        await page.waitForSelector("#MemberTimetable");
+
+        const data = await page.evaluate(() => {
+            const rows = document.querySelectorAll('table#MemberTimetable tr');
+            const result = [];
+            let day = null;
+            rows.forEach((r) => {
+                if (r.className === "dayHeader") {
+                    day = r.innerText.trim();
+                } else if (r.className !== "header") {
+                    const time = r.querySelector(".col0Item").innerText.trim();
+                    const name = r.querySelector(".col1Item").innerText.trim();
+                    const instructor = r.querySelector(".col3Item").innerText.trim();
+                    const duration = r.querySelector(".col4Item").innerText.trim();
+                    const status = r.querySelector("td:nth-last-of-type(1)").innerText.trim();
+                    let bookId = null;
+                    if (status !== "Past"){
+                        bookId = r.querySelector('td:nth-last-of-type(1) a').getAttribute("id");
+                    }
+                    result.push({
+                        bookId,
+                        status,
+                        day,
+                        time,
+                        name,
+                        instructor,
+                        duration
+                    })
+                }
+            });
+            return result;
+        });
+
+        console.dir(data.slice(0,10), {color: true});
+
+
+
+        // await page.click("a#slot134384");
+        // await page.waitForSelector("#totalAmount");
+        // await page.click("#btnPayNow");
 
     } catch (e) {
         await criticalException(e);
     }
 
-    // await page.screenshot({path: "full.png", fullPage: true});
     // await browser.close();
 })();
 
